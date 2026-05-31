d6d507fb3a8171aaf6827d784b4ba55eab839820
Brian Lalonde <brianary@gmail.com>
2026-05-30 22:38:10 -0700

    ✅🔨 Refactor dev scripts and tests

--- test/Connect-SshKey.Tests.ps1
+++ test/Connect-SshKey.Tests.ps1
@@ -6,7 +6,11 @@ Tests using OpenSSH to generate a key and connect it to an ssh server.
 $basename = "$(($MyInvocation.MyCommand.Name -split '\.',2)[0])."
 $skip = !(Test-Path .changes -Type Leaf) ? $false :
 	!@(Get-Content .changes |Get-Item |Select-Object -ExpandProperty Name |Where-Object {$_.StartsWith($basename)})
-if($skip) {Write-Information "No changes to $basename" -infa Continue}
+if(!(&"$PSScriptRoot/../scripts/Test-RelevantTest.ps1")) {return}
+BeforeAll {
+	Set-StrictMode -Version Latest
+	&"$PSScriptRoot/../scripts/Import-ThisModule.ps1"
+}
 Describe 'Connect-SshKey' -Tag Connect-SshKey -Skip:$skip {
 	BeforeAll {
 		$scriptsdir,$sep = (Split-Path $PSScriptRoot),[io.path]::PathSeparator
--- test/ConvertTo-BasicAuthentication.Tests.ps1
+++ test/ConvertTo-BasicAuthentication.Tests.ps1
@@ -8,7 +8,11 @@ Justification='These are tests.')] Param()
 $basename = "$(($MyInvocation.MyCommand.Name -split '\.',2)[0])."
 $skip = !(Test-Path .changes -Type Leaf) ? $false :
 	!@(Get-Content .changes |Get-Item |Select-Object -ExpandProperty Name |Where-Object {$_.StartsWith($basename)})
-if($skip) {Write-Information "No changes to $basename" -infa Continue}
+if(!(&"$PSScriptRoot/../scripts/Test-RelevantTest.ps1")) {return}
+BeforeAll {
+	Set-StrictMode -Version Latest
+	&"$PSScriptRoot/../scripts/Import-ThisModule.ps1"
+}
 Describe 'ConvertTo-BasicAuthentication' -Tag ConvertTo-BasicAuthentication -Skip:$skip {
 	BeforeAll {
 		$scriptsdir,$sep = (Split-Path $PSScriptRoot),[io.path]::PathSeparator
